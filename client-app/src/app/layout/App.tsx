bbbeb712b017de490e30329ec17dc804c9956d43
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-10 16:30:20 -0400

    Updating app component

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+  const [editMode, setEditMode] = useState(false)
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data)
+    })
+  }, [])
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  const handleCancelActivity = () => {
+    setSelectedActivity(undefined)
+  }
+
+  const handleFormOpen = (id?: string) => {
+    id ? handleSelectActivity(id) : handleCancelActivity();
+    setEditMode(true);
+  } 
+
+  const handleFormClose = () => {
+    setEditMode(false)
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+    </>
+  );
+}
+
+export default App;
