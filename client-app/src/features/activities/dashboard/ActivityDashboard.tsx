b90b81a499a561470f88d2d5dbc401a156f31b25
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-11 15:30:08 -0400

    Enabling form submit and edit

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -13,6 +13,7 @@ interface Props {
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
 }
 
 const ActivityDashboard: React.FC<Props> = (props) => {
@@ -25,14 +26,20 @@ const ActivityDashboard: React.FC<Props> = (props) => {
                 />
             </Grid.Column>
             <Grid.Column width="6">
-                {props.selectedActivity &&  !props.editMode &&(
+                {props.selectedActivity && !props.editMode && (
                     <ActivityDetails
                         activity={props.selectedActivity}
                         cancelSelectActivity={props.cancelSelectActivity}
                         openForm={props.openForm}
                     />
                 )}
-                {props.editMode && <ActivityForm  closeForm={props.closeForm} activity={props.selectedActivity}/>} 
+                {props.editMode && (
+                    <ActivityForm
+                        closeForm={props.closeForm}
+                        activity={props.selectedActivity}
+                        createOrEdit={props.createOrEdit}
+                    />
+                )}
             </Grid.Column>
         </Grid>
     );
