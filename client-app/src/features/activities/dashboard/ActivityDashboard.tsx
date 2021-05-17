ab1de7d4f8c8598422f466f39a685a59b7277d47
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-17 01:12:06 -0400

    Adding submitting

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 const ActivityDashboard: React.FC<Props> = (props) => {
@@ -25,6 +26,7 @@ const ActivityDashboard: React.FC<Props> = (props) => {
                     activities={props.activities}
                     selectActivity={props.selectActivity}
                     deleteActivity={props.deleteActivity}
+                    submitting={props.submitting}
                 />
             </Grid.Column>
             <Grid.Column width="6">
@@ -40,6 +42,7 @@ const ActivityDashboard: React.FC<Props> = (props) => {
                         closeForm={props.closeForm}
                         activity={props.selectedActivity}
                         createOrEdit={props.createOrEdit}
+                        submitting={props.submitting}
                     />
                 )}
             </Grid.Column>
