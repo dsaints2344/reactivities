27a156d56c15352cd471a19921ae5e42d275b10d
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-17 01:12:26 -0400

    Formatting and handling delete target

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import { Button, Item, Label, Segment } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import React, { useState } from "react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
 
-interface Props{
-    activities: Activity[]
-    selectActivity: (id: string) => void
+interface Props {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 const ActivityList: React.FC<Props> = (props) => {
-    return(
+    const[target, setTarget] = useState('')
+
+    const handleActivityDelete = (e: React.SyntheticEvent<HTMLButtonElement>, id: string) => {
+        setTarget(e.currentTarget.name)
+        props.deleteActivity(id)
+    }
+    return (
         <Segment>
             <Item.Group divided>
-                {props.activities.map(activity => (
+                {props.activities.map((activity) => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as="a">{activity.title}</Item.Header>
                             <Item.Meta>{activity.date}</Item.Meta>
                             <Item.Description>
                                 <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
+                                <div>
+                                    {activity.city}, {activity.venue}
+                                </div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => props.selectActivity(activity.id)} floated='right' content='View' color="blue"/>
-                                <Button onClick={() => props.deleteActivity(activity.id)} floated='right' content='Delete' color="red"/>
-                                <Label basic content={activity.category}/>
+                                <Button
+                                    onClick={() => props.selectActivity(activity.id)}
+                                    floated="right"
+                                    content="View"
+                                    color="blue"
+                                />
+                                <Button
+                                    name={activity.id}
+                                    loading={props.submitting && target === activity.id}
+                                    onClick={(e) => handleActivityDelete(e, activity.id)}
+                                    floated="right"
+                                    content="Delete"
+                                    color="red"
+                                />
+                                <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
                     </Item>
                 ))}
             </Item.Group>
         </Segment>
-    )
-}
+    );
+};
 
-export default ActivityList
+export default ActivityList;
