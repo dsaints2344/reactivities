61723880f66598f1c450313c0ade110348e11726
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-27 09:53:14 -0400

    Refactoring code

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,13 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
 import { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 
 const ActivityDashboard = () => {
     const { activityStore } = useStore()
-    const { selectedActivity, editMode } = activityStore
 
     useEffect(() => {
         activityStore.loadActivities();
@@ -24,12 +20,7 @@ const ActivityDashboard = () => {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width="6">
-                {selectedActivity && !editMode && (
-                    <ActivityDetails />
-                )}
-                {editMode && (
-                    <ActivityForm />
-                )}
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     );
