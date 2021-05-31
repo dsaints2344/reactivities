2e0516d7e85e1a267ccc60ed13513770b6ebae30
David De Los Santos <ddelossantosmateo@gmail.com>
2021-05-31 11:47:21 -0400

    Styling activity list and list item

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,54 +1,54 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Item, Label } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
-import { useStore } from '../../../app/stores/store'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
 interface Props {
-    activity: Activity
+    activity: Activity;
 }
 
-const ActivityItemList = ({activity}: Props) => {
-    const {activityStore} = useStore();
-    const{deleteActivity, loading} = activityStore;
-    const[target, setTarget] = useState('')
+const ActivityItemList = ({ activity }: Props) => {
+    const { activityStore } = useStore();
+    const { deleteActivity, loading } = activityStore;
+    const [target, setTarget] = useState("");
 
-    const handleActivityDelete = (e: React.SyntheticEvent<HTMLButtonElement>, id: string) => {
-        setTarget(e.currentTarget.name)
-        deleteActivity(id)
-    }
+    const handleActivityDelete = (
+        e: React.SyntheticEvent<HTMLButtonElement>,
+        id: string
+    ) => {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    };
     return (
-        <Item key={activity.id}>
-            <Item.Content>
-                <Item.Header as="a">{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>
-                        {activity.city}, {activity.venue}
-                    </div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button
-                        as={Link}
-                        to={`/activities/${activity.id}`}
-                        floated="right"
-                        content="View"
-                        color="blue"
-                    />
-                    <Button
-                        name={activity.id}
-                        loading={loading && target === activity.id}
-                        onClick={(e) => handleActivityDelete(e, activity.id)}
-                        floated="right"
-                        content="Delete"
-                        color="red"
-                    />
-                    <Label basic content={activity.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
-    )
-}
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size="tiny" circular src="/assets/user.png" />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+        </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button as={Link} to={`/activities/${activity.id}`} color='teal' floated='right' content='View' />
+            </Segment>
+        </Segment.Group>
+    );
+};
 
-export default ActivityItemList
+export default ActivityItemList;
