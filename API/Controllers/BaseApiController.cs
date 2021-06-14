bb446bd75640567f90408d0f2b42357c56c27009
David De Los Santos <ddelossantosmateo@gmail.com>
2021-06-13 23:40:52 -0400

    Handleling responses

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -15,6 +15,7 @@ namespace API.Controllers
 
         protected ActionResult HandleResult<T>(Result<T> result)
         {
+            if (result == null) return NotFound();
             if (result.IsSucess && result.Value != null) return Ok(result.Value);
             if (result.IsSucess && result.Value == null) return NotFound();
             return BadRequest(result.Error);
