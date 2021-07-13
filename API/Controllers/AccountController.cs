8680856deb7e605202c3737e9a7317791e8f29f7
David De Los Santos <ddelossantosmateo@gmail.com>
2021-07-13 00:29:37 -0400

    Creating account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using API.Controllers.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly TokenService _token;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, TokenService token)
+        {
+            _token = token;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _token.CreateToken(user),
+                    Username = user.UserName
+                };
+            }
+
+            return Unauthorized();
+
+        }
+    }
+}
