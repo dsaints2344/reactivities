ff86f124a25a61df6bdca443613d9dc69e8d01a3
David De Los Santos <ddelossantosmateo@gmail.com>
2021-07-20 10:42:15 -0400

    Adding login to home Page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Header, Segment, Image, Button } from 'semantic-ui-react'
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
 const HomePage = () => {
+    const { userStore } = useStore();
     return (
-        <Segment inverted textAlign= "center" vertical className='masthead'>
+        <Segment inverted textAlign="center" vertical className="masthead">
             <Container text>
                 <Header as="h1" inverted>
-                    <Image size="massive" src='/assets/logo.png' alt="logo" style={{marginBottom: 12}}/>
+                    <Image
+                        size="massive"
+                        src="/assets/logo.png"
+                        alt="logo"
+                        style={{ marginBottom: 12 }}
+                    />
                     Reactivities
                 </Header>
-                <Header as="h2" inverted content="Welcome to Reactivities"/>
-                <Button as={Link} to='/login' size="huge" inverted>
-                    Login!
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as="h2" inverted content="Welcome to Reactivities" />
+                        <Button as={Link} to="/activities" size="huge" inverted>
+                            Go to Activities!
+                        </Button>
+                    </>
+                ) : (
+                    <Button as={Link} to="/login" size="huge" inverted>
+                        Login!
+                    </Button>
+                )}
             </Container>
         </Segment>
-    )
-}
-export default HomePage
+    );
+};
+export default observer(HomePage);
