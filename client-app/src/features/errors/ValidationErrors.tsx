6f360983b262aa236b868be5ca2536379d60aac1
David De Los Santos <ddelossantosmateo@gmail.com>
2021-07-23 15:27:28 -0400

    Making values any

--- client-app/src/features/errors/ValidationErrors.tsx
+++ client-app/src/features/errors/ValidationErrors.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Message } from 'semantic-ui-react';
 
 interface Props {
-    errors: string[] | null
+    errors: any
 }
 
 const ValidationErrors: React.FC<Props> = (props) => {
@@ -10,7 +10,7 @@ const ValidationErrors: React.FC<Props> = (props) => {
         <Message error>
             {props.errors && (
                 <Message.List>
-                    {props.errors.map((err: any, i) => (
+                    {props.errors.map((err: any, i: any) => (
                         <Message.Item key={i}>
                             {err}
                         </Message.Item>
