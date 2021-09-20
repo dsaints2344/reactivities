de2a2b926cbefcaded5cf39fd9025b6d66fc4bcb
David De Los Santos <ddelossantosmateo@gmail.com>
2021-09-20 01:10:24 -0400

    Creating dropzone for photos and profile picture component

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,25 +1,57 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Card, Header, Tab, Image } from 'semantic-ui-react';
-import { Profile } from '../../app/models/profile';
+import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
+import {
+    Card,
+    Header,
+    Tab,
+    Image,
+    Grid,
+    Button,
+    GridColumn,
+} from "semantic-ui-react";
+import PhotoUploadWidget from "../../app/common/imageUpload/PhotoUploadWidget";
+import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
 
 interface Props {
     profile: Profile;
 }
 
-const ProfilePhotos: React.FC<Props> = ({profile}) => {
-    return(
+const ProfilePhotos: React.FC<Props> = ({ profile }) => {
+    const [addPhoneMode, setAddPhotoMode] = useState<boolean>(false);
+    const {
+        profileStore: { isCurrentUser },
+    } = useStore();
+    return (
         <Tab.Pane>
-            <Header icon='image' content='Photos'/>
-            <Card.Group itemsPerRow={5}>
-                {profile.photos?.map(photo => (
-                    <Card key={photo.id}>
-                        <Image src={photo.url}/>
-                    </Card>
-                ))}
-            </Card.Group>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated="left" icon="image" content="Photos" />
+                    {isCurrentUser && (
+                        <Button
+                            floated="right"
+                            basic
+                            content={addPhoneMode ? "Cancel" : "Add Poto"}
+                            onClick={() => setAddPhotoMode(!addPhoneMode)}
+                        />
+                    )}
+                </Grid.Column>
+                <GridColumn width={16}>
+                    {addPhoneMode ? (
+                        <PhotoUploadWidget/>
+                    ) : (
+                        <Card.Group itemsPerRow={5}>
+                            {profile.photos?.map((photo) => (
+                                <Card key={photo.id}>
+                                    <Image src={photo.url} />
+                                </Card>
+                            ))}
+                        </Card.Group>
+                    )}
+                </GridColumn>
+            </Grid>
         </Tab.Pane>
-    )
-}
+    );
+};
 
-export default observer(ProfilePhotos)
+export default observer(ProfilePhotos);
