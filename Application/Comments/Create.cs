3697ecd7aaa23ab20171955986fac34270239381
David De Los Santos <ddelossantosmateo@gmail.com>
2021-10-03 00:43:13 -0400

    Adding comment handler

--- Application/Comments/Create.cs
+++ Application/Comments/Create.cs
@@ -1,7 +1,14 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace Application.Comments
 {
@@ -18,6 +25,45 @@ namespace Application.Comments
             public CommandValidator()
             {
                 RuleFor(x => x.Body).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<CommentDto>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _mapper = mapper;
+                _context = context;
+
+            }
+            public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
+
+                if (activity == null) return null;
+
+                var user = await _context.Users
+                    .Include(p => p.Photos)
+                    .SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+                
+                var comment = new Comment
+                {
+                    Author = user,
+                    Activity = activity,
+                    Body = request.Body
+                };
+
+                activity.Comments.Add(comment);
+                
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment));
+
+                return Result<CommentDto>.Failure("Failed to add comment");
             }
         }
     }
