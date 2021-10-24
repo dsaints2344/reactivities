22e498c36de503f699cbc0152d1ee3f6c4078f11
David De Los Santos <ddelossantosmateo@gmail.com>
2021-10-24 19:49:37 -0400

    Adding profile followings tab

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,24 +1,35 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Tab } from 'semantic-ui-react';
-import { Profile } from '../../app/models/profile';
-import ProfilePhotos from './ProfilePhotos';
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Tab } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import ProfileFollowings from "./ProfileFollowings";
+import ProfilePhotos from "./ProfilePhotos";
 
 interface Props {
     profile: Profile;
 }
-const ProfileContent: React.FC<Props> = ({profile}) => {
+const ProfileContent: React.FC<Props> = ({ profile }) => {
+    const { profileStore } = useStore();
+
     const panes = [
-        {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
-        {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile}/>},
-        {menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane>},
-        {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
-        {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>},
+        { menuItem: "About", render: () => <Tab.Pane>About Content</Tab.Pane> },
+        { menuItem: "Photos", render: () => <ProfilePhotos profile={profile} /> },
+        { menuItem: "Events", render: () => <Tab.Pane>Events Content</Tab.Pane> },
+        { menuItem: "Followers", render: () => <ProfileFollowings /> },
+        { menuItem: "Following", render: () => <ProfileFollowings /> },
     ];
-    
-    return(
-        <Tab menu={{fluid: true, vertical: true}} menuPosition='right' panes={panes}/>
-    )
-}
+
+    return (
+        <Tab
+            menu={{ fluid: true, vertical: true }}
+            menuPosition="right"
+            panes={panes}
+            onTabChange={(e, data) =>
+                profileStore.setActiveTab(Number(data.activeIndex))
+            }
+        />
+    );
+};
 
 export default observer(ProfileContent);
