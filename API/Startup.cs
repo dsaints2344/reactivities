e3c82d779a9d87b310f337e0ef6f6c858c825f09
David De Los Santos <ddelossantosmateo@gmail.com>
2021-12-06 15:16:42 -0400

    Fix image source typo

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,8 +64,8 @@ namespace API
                 .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
-                .FrameAncestors(s => s.Self().CustomSources("https://res.cloudinary.com"))
-                .ImageSources(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-cfPjyNG99e7U321NguHOqQsWHkzKSxuHfUaquh7DCtg="))
                 );
 
